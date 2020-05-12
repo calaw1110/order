379ab3e72dfce62d798426a0f1af7e4c6db9e4ae
calaw1110 <calaw1110@gmail.com>
2020-05-12 23:09:01 +0800

    缺少結帳明細

---
+++ Meals/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Meals
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdBtn2 = new System.Windows.Forms.RadioButton();
+            this.rdBtn1 = new System.Windows.Forms.RadioButton();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(132)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(13, 23);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(363, 477);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(175)))), ((int)(((byte)(225)))), ((int)(((byte)(255)))));
+            this.panel2.Controls.Add(this.groupBox1);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.groupBox2);
+            this.panel2.Location = new System.Drawing.Point(16, 15);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(333, 447);
+            this.panel2.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.groupBox1.Controls.Add(this.rdBtn2);
+            this.groupBox1.Controls.Add(this.rdBtn1);
+            this.groupBox1.Font = new System.Drawing.Font("標楷體", 14F);
+            this.groupBox1.Location = new System.Drawing.Point(15, 286);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(204, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "外帶/內用";
+            // 
+            // rdBtn2
+            // 
+            this.rdBtn2.AutoSize = true;
+            this.rdBtn2.Location = new System.Drawing.Point(10, 65);
+            this.rdBtn2.Margin = new System.Windows.Forms.Padding(5);
+            this.rdBtn2.Name = "rdBtn2";
+            this.rdBtn2.Size = new System.Drawing.Size(187, 23);
+            this.rdBtn2.TabIndex = 1;
+            this.rdBtn2.TabStop = true;
+            this.rdBtn2.Text = "內用(每人加10元)";
+            this.rdBtn2.UseVisualStyleBackColor = true;
+            this.rdBtn2.CheckedChanged += new System.EventHandler(this.rdBtn2_CheckedChanged);
+            // 
+            // rdBtn1
+            // 
+            this.rdBtn1.AutoSize = true;
+            this.rdBtn1.Location = new System.Drawing.Point(10, 31);
+            this.rdBtn1.Margin = new System.Windows.Forms.Padding(5);
+            this.rdBtn1.Name = "rdBtn1";
+            this.rdBtn1.Size = new System.Drawing.Size(67, 23);
+            this.rdBtn1.TabIndex = 0;
+            this.rdBtn1.TabStop = true;
+            this.rdBtn1.Text = "外帶";
+            this.rdBtn1.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("標楷體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label8.Location = new System.Drawing.Point(24, 407);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 19);
+            this.label8.TabIndex = 16;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("標楷體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(228, 347);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "取消";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("標楷體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(228, 294);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "結帳";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Font = new System.Drawing.Font("標楷體", 14F);
+            this.groupBox2.Location = new System.Drawing.Point(3, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(327, 273);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label7.Font = new System.Drawing.Font("標楷體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label7.Location = new System.Drawing.Point(280, 235);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 19);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "人";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label6.Font = new System.Drawing.Font("標楷體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.Location = new System.Drawing.Point(280, 201);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 19);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "份";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("標楷體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(280, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 19);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "杯";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label5.Font = new System.Drawing.Font("標楷體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(280, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 19);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "份";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label2.Font = new System.Drawing.Font("標楷體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(280, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 19);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "杯";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label4.Font = new System.Drawing.Font("標楷體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(280, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 19);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "杯";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label3.Font = new System.Drawing.Font("標楷體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(280, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 19);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "杯";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 522);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdBtn2;
+        private System.Windows.Forms.RadioButton rdBtn1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Meals/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//練習用 物件屬性宣告  get set
+
+
+
+
+namespace Meals
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Create();
+            label8.Text = "歡迎光臨！";
+            label7.Visible = false;
+            rdBtn1.Checked = false;
+            rdBtn2.Checked = false;
+        }
+        int sum,n;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sum = 0;
+            for(int i =0;i<Chbox.Length; i++)
+            {
+                if (Chbox[i].Checked == true)
+                {
+                    sum += DrinkPrice[i] * Convert.ToInt32(Tbox[i].Text);
+                }
+            }
+            label8.Text = "計：" + sum + "元";
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {//清除所有勾選、數字
+            for (int i = 0; i < 6; i++)
+            {
+                Tbox[i].Text = "";
+                Chbox[i].Checked=false;
+            }
+            rdBtn1.Checked = false;
+            rdBtn2.Checked = false;
+        }
+        //選到內用時
+        private void rdBtn2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdBtn2.Checked == true)
+            {
+                Chbox[6].Visible = true;
+                Tbox[6].Visible = true;
+                label7.Visible = true;
+                Tbox[6].Text = "1";
+                Chbox[6].Checked = true;
+            }
+            else
+            {
+                Chbox[6].Visible = false;
+                Tbox[6].Visible = false;
+                label7.Visible = false;
+                Tbox[6].Text = "";
+                Chbox[6].Checked = false;
+            }
+        }
+        CheckBox[] Chbox = new CheckBox[7];
+        TextBox[] Tbox = new TextBox[7];
+        Drinks Drink = new Drinks();
+        string[] DrinkName = new string[7] { "泡沫紅茶 ", "珍珠奶茶 ", "木瓜牛奶 ", "柳橙汁   ", "漢堡A餐 ", "麥克雞塊", "內用每人 " };
+        int[] DrinkPrice = new int[7] { 30, 35, 40, 40, 100, 120, 10 };
+        private void Create()
+        {
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            for (int i =0;i<7;i++) {
+                Chbox[i] = new CheckBox();
+                Chbox[i].Name = string.Format("cbox_{0}",i);
+                Chbox[i].SetBounds(18,23 +35*i,190,25);
+                Chbox[i].Text = DrinkName[i] + "  " + DrinkPrice[i]+"元";
+                Chbox[i].CheckedChanged += Chbox_CheckedChanged;
+
+                Tbox[i] = new TextBox();
+                Tbox[i].Name = string.Format("tbox_{0}", i);
+                Tbox[i].SetBounds(219, 23 + 35 * i, 50, 35);
+                Tbox[i].TextChanged += Tbox_TextChanged;
+                Tbox[i].TextAlign= HorizontalAlignment.Right;       //**增
+
+                groupBox2.Controls.Add(Chbox[i]);
+                groupBox2.Controls.Add(Tbox[i]);
+                
+            }
+            
+            Chbox[6].Visible = false;
+            Tbox[6].Visible = false;
+        }
+
+        private void Tbox_TextChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Tbox.Length; i++)
+            {
+                if (int.TryParse(Tbox[i].Text, out n) == true && n > 0)
+                {
+                     Chbox[i].Checked = true;
+                }
+                else 
+                {
+                    Chbox[i].Checked = false;
+                    if (int.TryParse(Tbox[i].Text, out n) == false && Tbox[i].Text != "")
+                        Tbox[i].Text = "";
+                }
+            }
+           
+        }
+
+        private void Chbox_CheckedChanged(object sender, EventArgs e)
+        {
+            string s=((CheckBox)sender).Name;
+            int x = int.Parse(s.Substring(s.Length - 1, 1));
+            if (Chbox[x].Checked == false)
+                Tbox[x].Text = "";
+            else
+                if (Tbox[x].Text.Trim() == "")
+                    Tbox[x].Text = "1";
+
+
+            //**以上段替代下段，可免使用迴圈
+            //for (int i = 0; i < Chbox.Length; i++) 
+            //    if (Chbox[i].Checked ==false)
+            //        Tbox[i].Text = "";
+        }
+
+    }
+
+
+    public class Drinks
+    {
+        public string DrinkName { set; get; }
+        public int DrinkPrice { set; get; }
+
+    }
+
+    
+}
